38e03aa93ca85e052268df04364f22df19acb13f
Regivaldo Silva <regivaldorfs@gmail.com>
2024-04-19 10:23:32 -0300

    Ajustes

--- src/js/whereami.js
+++ src/js/whereami.js
@@ -1,23 +1,37 @@
-const createBorder = (enviroment = "") => {
-	const element = document.createElement("div");
+const createBorder = (enviroment = '') => {
+  const element = document.createElement('div')
 
-	element.classList.add("whereami-flags");
-	element.classList.add("stage");
-	element.title = `Você está no ambiente de ${enviroment}`;
-	element.id = "whereami";
+  element.classList.add('whereami-flags')
+  element.classList.add('stage')
+  element.title = `Você está no ambiente de ${enviroment}`
+  element.id = 'whereami'
 
-	return element;
-};
+  return element
+}
 
 const hasConfiguration = () => {
-	return false;
-};
+  return false
+}
 
 const verify = () => {
-	if (!hasConfiguration()) {
-		const body = document.querySelector("body");
-		body.append(createBorder("Desenvolvimento"));
-	}
-};
+  if (!hasConfiguration()) {
+    const body = document.querySelector('body')
+    const url = location.url
 
-verify();
+    var db = connect.result
+    var tx = db.transaction('links', 'readonly')
+    var store = tx.objectStore('links')
+    var request = store.getAll()
+    var result
+
+    request.onsuccess = function (event) {
+      result = event.target.result.filter((u) => u.url.include(url))
+    }
+
+    if (result) {
+      body.append(createBorder('Desenvolvimento'))
+    }
+  }
+}
+
+verify()

e864a5a8848d52d074f4660c3424a6e833fc3f87
Regivaldo Silva <regivaldorfs@gmail.com>
2025-02-27 22:15:19 -0300

    revert

--- src/js/whereami.js
+++ src/js/whereami.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ;(function (doc) {
   'use strict'
 
@@ -35,3 +36,28 @@
 
   doc.body.appendChild(div)
 })(document)
+=======
+const createBorder = (enviroment = "") => {
+	const element = document.createElement("div");
+
+	element.classList.add("whereami-flags");
+	element.classList.add("stage");
+	element.title = `Você está no ambiente de ${enviroment}`;
+	element.id = "whereami";
+
+	return element;
+};
+
+const hasConfiguration = () => {
+	return false;
+};
+
+const verify = () => {
+	if (!hasConfiguration()) {
+		const body = document.querySelector("body");
+		body.append(createBorder("Desenvolvimento"));
+	}
+};
+
+verify();
+>>>>>>> parent of 38e03aa (Ajustes)
